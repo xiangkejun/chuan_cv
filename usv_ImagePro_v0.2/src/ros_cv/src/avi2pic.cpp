02a3570244a8654994ada141215abb0d490be726
xiangkejun <42697766+xiangkejun@users.noreply.github.com>
2019-05-12 10:00:18 +0800

    Update avi2pic.cpp

--- usv_ImagePro_v0.2/src/ros_cv/src/avi2pic.cpp
+++ usv_ImagePro_v0.2/src/ros_cv/src/avi2pic.cpp
@@ -1,44 +1,73 @@
-#include <iostream>
-#include "cv.h"
-#include "opencv2/opencv.hpp"
+// xx
+// 订阅/camera/image后，保存为.avi 
 
-using namespace std;
-using namespace cv;
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-// 描述：将视频帧转成图片输出
+static const std::string OPENCV_WINDOW = "Image window";
+
+cv::VideoWriter writer("/home/xx/Videos/write.avi",CV_FOURCC('M','J','P','G'),30.0,cv::Size(640,480));
+
+class ImageConverter
+{
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+//  image_transport::Publisher image_pub_;
+
+public:
+  ImageConverter()
+    : it_(nh_)
+  {
+    // Subscrive to input video feed and publish output video feed
+    image_sub_ = it_.subscribe("/camera/image", 1,
+      &ImageConverter::imageCb, this);
+//    image_pub_ = it_.advertise("/image_converter/output_video", 1);
+
+    cv::namedWindow(OPENCV_WINDOW);
+  }
+
+  ~ImageConverter()
+  {
+    cv::destroyWindow(OPENCV_WINDOW);
+  }
+
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+   writer<<cv_ptr->image;
+   std::cout<<"video is saving to /home/xx/Videos/write.avi ..."<<std::endl;
+    // Draw an example circle on the video stream
+    // if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+    //   cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+
+    // Update GUI Window
+    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+    cv::waitKey(3);
+
+    // Output modified video stream
+   // image_pub_.publish(cv_ptr->toImageMsg());
+  }
+};
+
 int main(int argc, char** argv)
 {
-	// 获取视频文件
-	VideoCapture cap("/home/xx/video_xx/tushu1.avi");
-
-	// 获取视频总帧数
-	long totalFrameNumber = cap.get(CV_CAP_PROP_FRAME_COUNT);
-	cout << "total frames: " << totalFrameNumber << endl;
-
-	Mat frame;
-	bool flags = true;
-	long currentFrame = 0;
-
-	while (flags){
-		// 读取视频每一帧
-		cap.read(frame);
-
-		stringstream str;
-		str << "cqh" << currentFrame << ".jpg";
-		cout << "正在处理第" << currentFrame << "帧" << endl;
-		printf("\n");
-
-		// 设置每30帧获取一次帧
-		if (currentFrame % 30 == 0){
-			// 将帧转成图片输出
-			imwrite("/home/xx/video_xx/trans" + str.str(), frame);
-		}
-		// 结束条件
-		if (currentFrame >= totalFrameNumber){
-			flags = false;
-		}
-		currentFrame++;
-	}
-	
-	system("pause");
+  ros::init(argc, argv, "image_converter");
+  ImageConverter ic;
+  ros::spin();
+  return 0;
 }
